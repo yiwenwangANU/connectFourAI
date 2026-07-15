6fd55cba0974c8f90d7845945ce7d5c471eb7224
Yiwen Wang <wangyiwenau@gmail.com>
2026-07-15 12:46:09 +1000

    feat: add different server routes

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,30 @@ import Fastify from "fastify";
 
 const server = Fastify();
 
+server.get("/", async () => {
+  return "hello wolrd";
+});
+
+server.get<{
+  Headers: { myheader: boolean };
+  Querystring: { id: number };
+  Reply: {
+    200: { status: string; id: number; myHeader: boolean };
+    500: { code: number };
+  };
+}>("/employee", async (req, rep) => {
+  try {
+    return rep.status(200).send({
+      status: "success",
+      id: req.query.id,
+      myHeader: req.headers.myheader,
+    });
+  } catch (err) {
+    req.log.error(err);
+    return rep.status(500).send({ code: 500 });
+  }
+});
+
 server.listen({ port: 8080, host: "::1" }, (err, address) => {
   if (err) {
     console.error(err);
