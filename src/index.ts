6d08183ab785cab4692c80eb26d168a03267a75c
Yiwen Wang <wangyiwenau@gmail.com>
2026-07-15 13:28:08 +1000

    feat: more routing

--- src/index.ts
+++ src/index.ts
@@ -9,21 +9,27 @@ server.get("/", async () => {
 server.get<{
   Headers: { myheader: boolean };
   Querystring: { id: number };
+  Params: { param: number };
   Reply: {
-    200: { status: string; id: number; myHeader: boolean };
+    200: { status: string; id: number; param: number; myHeader: boolean };
     500: { code: number };
   };
-}>("/employee", async (req, rep) => {
+}>("/employee/:param", async (req, rep) => {
   try {
     return rep.status(200).send({
       status: "success",
       id: req.query.id,
+      param: req.params.param,
       myHeader: req.headers.myheader,
     });
   } catch (err) {
     req.log.error(err);
     return rep.status(500).send({ code: 500 });
   }
+});
+
+server.post<{ Body: { userName: string } }>("/employee", (req) => {
+  return req.body.userName;
 });
 
 server.listen({ port: 8080, host: "::1" }, (err, address) => {
